010bdedeca8f0f623e8ae51f18913aefdd6e13ce
everestp <paudeleverest09@gmail.com>
2026-01-05 14:53:56 +0545

    16 .Add sync.Mutex to our code

--- mutex/main.go
+++ mutex/main.go
@@ -9,16 +9,19 @@ var msg string
 var wg sync.WaitGroup
 
 
-func updateMessage(s string){
+func updateMessage(s string , m *sync.Mutex){
 	defer wg.Done()
+	m.Lock()
 	msg =s
+	m.Unlock()
 }
 
 func main(){
 	msg = "Hello world"
+	var mutex  sync.Mutex
 	wg.Add(2)
-	go updateMessage("hello universe")
-		go updateMessage("hello cosmos")
+	go updateMessage("hello universe", &mutex)
+		go updateMessage("hello cosmos",&mutex)
 		wg.Wait()
 		fmt.Println(msg)
 
